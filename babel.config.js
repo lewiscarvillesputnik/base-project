51e88e6e07c261d4b76ed0c9fca1044a45f59482
lewiscarville <46135124+lewiscarville@users.noreply.github.com>
2020-08-21 10:47:55 +0100

    Transpiling for all browsers - Babel

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,6 @@ module.exports = {
       '@babel/preset-env',
       {
         modules: false,
-        targets: {
-          browsers: 'Edge >= 15, Firefox >= 53, FirefoxAndroid  >= 53, Chrome >= 55, ChromeAndroid >= 55, Opera >= 42, OperaMobile >= 42, Safari >= 10.1, iOS >= 10.1',
-        },
       }
     ]
   ]
